72f58fd32cf22a7e353a2071667bc42ee4d445a1
Diogo Zago <diogozago04@gmail.com>
2024-06-14 20:56:15 -0300

    feat: Adicionado Appshell

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { AppShell, Burger, Skeleton, Group } from "@mantine/core";
+import { useDisclosure } from '@mantine/hooks';
+
 function App() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
-}
+const [opened, { toggle }] = useDisclosure();
+return (
+  <>
+    <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !opened, desktop: !opened },
+        }}
+        padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} size="sm" />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  </>
+)}
 
 export default App
